f98ce25b543642bc9c67168bc96e55223eb25dee
Ted <84779771+Ted-18@users.noreply.github.com>
2023-05-02 17:46:16 +0200

    1.0.2

--- Moderator/handlers/handlerDatabaseInit.py
+++ Moderator/handlers/handlerDatabaseInit.py
@@ -1,20 +1,39 @@
 import services.serviceDatabase as serviceDatabase
+import settings.settingBot as settingBot
 
 # Create the database if it does not exist
 def databaseInit():
-    # Table structure
-    tableName = "addon_moderator_banwords"
-    columns = [
-        ["serverID", "BIGINT NOT NULL"],
-        ["word", "VARCHAR(255) NOT NULL"]
-    ]
-    serviceDatabase.databaseCreation(tableName, columns)
+    if settingBot.databaseType == "MariaDB":
+        # Table structure
+        tableName = "addon_moderator_banwords"
+        columns = [
+            ["serverID", "BIGINT NOT NULL"],
+            ["word", "VARCHAR(255) NOT NULL"]
+        ]
+        serviceDatabase.databaseCreation(tableName, columns)
 
-    # Table structure
-    tableName = "addon_moderator_inviteshield"
-    columns = [
-        ["serverID", "BIGINT NOT NULL"],
-        ["status", "BOOLEAN DEFAULT 0"]
-    ]
-    serviceDatabase.databaseCreation(tableName, columns)
+        # Table structure
+        tableName = "addon_moderator_inviteshield"
+        columns = [
+            ["serverID", "BIGINT NOT NULL"],
+            ["status", "BOOLEAN DEFAULT 0"]
+        ]
+        serviceDatabase.databaseCreation(tableName, columns)
 
+
+    elif settingBot.databaseType == "SQLite":
+        # Table structure
+        tableName = "addon_moderator_banwords"
+        columns = [
+            ["serverID", "integer NOT NULL"],
+            ["word", "text NOT NULL"]
+        ]
+        serviceDatabase.databaseCreation(tableName, columns)
+
+        # Table structure
+        tableName = "addon_moderator_inviteshield"
+        columns = [
+            ["serverID", "integer NOT NULL"],
+            ["status", "integer DEFAULT 0"]
+        ]
+        serviceDatabase.databaseCreation(tableName, columns)
--- Moderator/init.py
+++ Moderator/init.py
@@ -2,7 +2,7 @@
 enableGithub = True
 author = "Ted-18"
 repository = "Bot.Assistant-Moderator"
-version = "1.0.1" 
+version = "1.0.2" 
 
 # To activate this addon
 cogEnabled = True
